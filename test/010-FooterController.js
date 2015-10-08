06b05afa8fa876d5aec74c0d35d454444f8e2752
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2015-10-08 12:17:28 +0200

    add basic karma conf

---
+++ test/010-FooterController.js
@@ -0,0 +1,21 @@
+describe('FooterController', function() {
+  beforeEach(module('theHive'));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_){
+      // The injector unwraps the underscores (_) from around the parameter names when matching
+      $controller = _$controller_;
+    }));
+
+    describe('FooterController', function() {
+      it('show Envinroment on footer', function() {
+        var controller = $controller('FooterController', {
+          'CONFIG': {
+            'ENV': 'test'
+          }
+        });
+        expect(controller.envLabel).toEqual(' - test');
+      });
+    });
+});
